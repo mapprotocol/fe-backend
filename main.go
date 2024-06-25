6a0e918c8df90ab1bb4ad8c024da7bb00656d1bd
neoiss <neoiss@163.com>
2024-06-25 15:10:33 +0800

    delete the useless files

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"github.com/fvbock/endless"
-	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 
 	"github.com/mapprotocol/fe-backend/config"
-	"github.com/mapprotocol/fe-backend/resource/ceffu"
 	"github.com/mapprotocol/fe-backend/resource/db"
 	"github.com/mapprotocol/fe-backend/resource/log"
-	"github.com/mapprotocol/fe-backend/router"
 )
 
 func main() {
@@ -20,12 +16,4 @@ func main() {
 	// init db
 	dbConf := viper.GetStringMapString("database")
 	db.Init(dbConf["user"], dbConf["password"], dbConf["host"], dbConf["port"], dbConf["name"])
-
-	// init ceffu client
-	ceffuConf := viper.GetStringMapString("ceffu")
-	ceffu.Init(ceffuConf["domain"], ceffuConf["key"], ceffuConf["secret"])
-
-	engine := gin.Default()
-	router.Register(engine)
-	_ = endless.ListenAndServe(viper.GetString("address"), engine)
 }
