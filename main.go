d04bda3326151cc82aa7948f7859c3a3f6c44162
mapdev33 <>
2024-08-14 10:49:11 +0800

    update the config file

--- main.go
+++ main.go
@@ -25,6 +25,77 @@ func version() string {
 	return fmt.Sprintf("%d.%d.%d-%s", VersionMajor, VersionMinor, VersionPatch, VersionMeta)
 }
 
+func toConfig() (*logic.CollectCfg, error) {
+	cfg := &logic.CollectCfg{}
+	cfg.Testnet = viper.GetBool("testnet")
+
+	network := &chaincfg.MainNetParams
+	if cfg.Testnet {
+		network = &chaincfg.TestNet3Params
+	}
+	cfg.StrHotWallet1Priv = viper.GetString("hotwalletPriv1")
+	cfg.StrHotWallet2Priv = viper.GetString("hotwalletPriv2")
+
+	cfg.StrHotWalletFee1Privkey = viper.GetString("hotwalletFeePriv1")
+	cfg.StrHotWalletFee2Privkey = viper.GetString("hotwalletFeePriv2")
+	cfg.StrFee3Privkey = viper.GetString("hotwalletFeePriv3")
+
+	strHotAddr1 := viper.GetString("hotwalletAddress1")
+	hotAddr1, err := btcutil.DecodeAddress(strHotAddr1, network)
+	if err != nil {
+		log.Logger().WithField("error", err).Error("decode hot1 address failed")
+		return cfg, err
+	}
+	cfg.HotWallet1 = hotAddr1
+
+	strHotAddr2 := viper.GetString("hotwalletAddress2")
+	hotAddr2, err := btcutil.DecodeAddress(strHotAddr2, network)
+	if err != nil {
+		log.Logger().WithField("error", err).Error("decode hot2 address failed")
+		return cfg, err
+	}
+	cfg.HotWallet2 = hotAddr2
+
+	strFeeAddr1 := viper.GetString("hotwalletFeeAddress1")
+	feeAddr1, err := btcutil.DecodeAddress(strFeeAddr1, network)
+	if err != nil {
+		log.Logger().WithField("error", err).Error("decode fee1 address failed")
+		return cfg, err
+	}
+	cfg.HotWalletFee1 = feeAddr1
+
+	strFeeAddr2 := viper.GetString("hotwalletFeeAddress2")
+	feeAddr2, err := btcutil.DecodeAddress(strFeeAddr2, network)
+	if err != nil {
+		log.Logger().WithField("error", err).Error("decode fee2 address failed")
+		return cfg, err
+	}
+	cfg.HotWalletFee2 = feeAddr2
+
+	strFeeAddr3 := viper.GetString("hotwalletFeeAddress3")
+	feeAddr3, err := btcutil.DecodeAddress(strFeeAddr3, network)
+	if err != nil {
+		log.Logger().WithField("error", err).Error("decode fee3 address failed")
+		return cfg, err
+	}
+	cfg.HotWalletFee3 = feeAddr3
+
+	amount0 := viper.GetFloat64("minHotWallet2Amount")
+	amount1, err := btcutil.NewAmount(amount0)
+	if err != nil {
+		return cfg, err
+	}
+	cfg.HotWallet2Line = int64(amount1)
+
+	amount0 = viper.GetFloat64("maxTranferAmount")
+	amount1, err = btcutil.NewAmount(amount0)
+	if err != nil {
+		return cfg, err
+	}
+	cfg.MaxTransferAmount = int64(amount1)
+
+	return cfg, nil
+}
 func main() {
 	args := os.Args
 	if len(args) >= 2 && args[1] == "version" {
@@ -41,27 +112,10 @@ func main() {
 	dbConf := viper.GetStringMapString("database")
 	db.Init(dbConf["user"], dbConf["password"], dbConf["host"], dbConf["port"], dbConf["name"])
 
-	testnet := viper.GetBool("testnet")
-	network := &chaincfg.MainNetParams
-	if testnet {
-		network = &chaincfg.TestNet3Params
-	}
-	feeAddress, err := btcutil.DecodeAddress(viper.GetString("feeaddress"), network)
+	cfg, err := toConfig()
 	if err != nil {
-		log.Logger().WithField("error", err).Error("decode fee address failed")
+		log.Logger().WithField("error", err).Error("read config failed")
 		return
-	}
-	receiverAddress, err := btcutil.DecodeAddress(viper.GetString("receiver"), network)
-	if err != nil {
-		log.Logger().WithField("error", err).Error("decode fee address failed")
-		return
-	}
-
-	cfg := &logic.CollectCfg{
-		Testnet:                 testnet,
-		StrHotWalletFee1Privkey: viper.GetString("feeprivatekey"),
-		HotWalletFee1:           feeAddress,
-		HotWallet1:              receiverAddress,
 	}
 	if err := logic.Run(cfg); err != nil {
 		log.Logger().WithField("error", err).Error("collect failed")
